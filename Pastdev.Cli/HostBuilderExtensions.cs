50ea7450ea8ba3b303a89ed3a83d0efd37f5d3f1
Lucas Theisen <lucastheisen@pastdev.com>
2018-10-14 11:10:20 -0400

    fixed namespaces

--- Pastdev.Cli/HostBuilderExtensions.cs
+++ Pastdev.Cli/HostBuilderExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Ross.Database.Migrate.Postgres
+namespace Pastdev.Cli
 {
     /// <summary>
     /// A placeholder until
--- Pastdev.Cli/Program.cs
+++ Pastdev.Cli/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 
-namespace Ross.Database.Migrate.Postgres
+namespace Pastdev.Cli
 {
     public class Program
     {
